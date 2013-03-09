a7390b98a71f7a2ff92e205b4fe1da974d32dd56
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-03-10 00:42:13 +0800

    Show Open Source logo

--- main/src/com/gamalinda/libgdx/test/MainApp.java
+++ main/src/com/gamalinda/libgdx/test/MainApp.java
@@ -1,12 +1,24 @@
 package com.gamalinda.libgdx.test;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MainApp implements ApplicationListener {
 
+    OrthographicCamera camera;
+    Texture openSourceLogo;
+    SpriteBatch spriteBatch;
+
     @Override
     public void create() {
-
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+        openSourceLogo = new Texture(Gdx.files.internal("res/images/opensource-logo.png"));
+        spriteBatch = new SpriteBatch();
     }
 
     @Override
@@ -16,7 +28,13 @@ public class MainApp implements ApplicationListener {
 
     @Override
     public void render() {
-
+        Gdx.gl.glClearColor(1f, 1f, 1f, 1f);
+        Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+        camera.update();
+        spriteBatch.setProjectionMatrix(camera.combined);
+        spriteBatch.begin();
+        spriteBatch.draw(openSourceLogo, 0, 0);
+        spriteBatch.end();
     }
 
     @Override
