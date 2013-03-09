e4c9b529d1947e876605a4a1c55b1e41d66bbf2a
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-03-10 01:53:24 +0800

    Externalized to Screen class the showing of image

--- main/src/com/gamalinda/libgdx/test/MainApp.java
+++ main/src/com/gamalinda/libgdx/test/MainApp.java
@@ -1,54 +1,37 @@
 package com.gamalinda.libgdx.test;
 
-import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL10;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
+import com.gamalinda.libgdx.test.screen.ShowImageScreen;
 
-public class MainApp implements ApplicationListener {
-
-    OrthographicCamera camera;
-    Texture openSourceLogo;
-    SpriteBatch spriteBatch;
+public class MainApp extends Game {
 
     @Override
     public void create() {
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, 800, 480);
-        openSourceLogo = new Texture(Gdx.files.internal("res/images/opensource-logo.png"));
-        spriteBatch = new SpriteBatch();
+        this.setScreen(new ShowImageScreen());
     }
 
     @Override
-    public void resize(int i, int i2) {
-
+    public void resize(int width, int height) {
+        super.resize(width, height);
     }
 
     @Override
     public void render() {
-        Gdx.gl.glClearColor(1f, 1f, 1f, 1f);
-        Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-        camera.update();
-        spriteBatch.setProjectionMatrix(camera.combined);
-        spriteBatch.begin();
-        spriteBatch.draw(openSourceLogo, 0, 0);
-        spriteBatch.end();
+        super.render();
     }
 
     @Override
     public void pause() {
-
+        super.pause();
     }
 
     @Override
     public void resume() {
-
+        super.resume();
     }
 
     @Override
     public void dispose() {
-
+        super.dispose();
     }
 }
---
+++ main/src/com/gamalinda/libgdx/test/screen/ShowImageScreen.java
@@ -0,0 +1,59 @@
+package com.gamalinda.libgdx.test.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class ShowImageScreen implements Screen {
+
+    OrthographicCamera camera;
+    Texture openSourceLogo;
+    SpriteBatch spriteBatch;
+
+    @Override
+    public void render(float v) {
+        Gdx.gl.glClearColor(1f, 1f, 1f, 1f);
+        Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+        camera.update();
+        spriteBatch.setProjectionMatrix(camera.combined);
+        spriteBatch.begin();
+        spriteBatch.draw(openSourceLogo, 0, 0);
+        spriteBatch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        camera.setToOrtho(false, width, height);
+    }
+
+    @Override
+    public void show() {
+        camera = new OrthographicCamera();
+        openSourceLogo = new Texture(Gdx.files.internal("res/images/opensource-logo.png"));
+        spriteBatch = new SpriteBatch();
+    }
+
+    @Override
+    public void hide() {
+        openSourceLogo.dispose();
+        spriteBatch.dispose();
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
